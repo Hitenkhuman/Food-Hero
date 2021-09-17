c6514e4330a98705dcbc92823fda32c238b7a330
Hiten <hiten1512tata@gmail.com>
2021-09-17 18:02:16 +0530

    gitignore change

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: "mongodb://localhost/foodhero",
+  url: "mongodb+srv://hiten15:Hiten1512@cluster0.1haxf.mongodb.net/test",
   img_path: "http://localhost:9000//profile_pic/",
 };
